8ca745bdd24f95426c92d0be40bac8e31e87d000
LittleLion <894140605@qq.com>
2021-03-10 00:17:06 +0800

    Finish LoginPage and RegisterPage and them change;

--- app/src/main/java/com/example/mymiddletest/MainActivity.java
+++ app/src/main/java/com/example/mymiddletest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mymiddletest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController controller = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this,controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController controller = Navigation.findNavController(this,R.id.fragment);
+        return controller.navigateUp();
     }
 }
